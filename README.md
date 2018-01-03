cace9e1e82696600c286d8397ff4ab3bc6ea270f
Thiago <thiago.engh@gmail.com>
2018-01-03 20:29:33 -0300

    Commit com alteracoes finais

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Requirments- Device runing Python 2.7
 
 Installing - download zip from repo: https://github.com/jpyces76/ud036_StarterCode unpack into desired folder. 
 
-Running - from command line: Python entertainment.py
+Running - from command line: Python entertainment_center.py
 
 from idle: open entertainment.py then -> Run -> Run module (F5)
--- media.py
+++ media.py
@@ -1,25 +1,26 @@
 import webbrowser
-#Class who abstract the movie
-class Movie(object):
 
-	"""This is a example showing init clss 
-    
+
+# noinspection PyInterpreter
+class Movie(object):
+    """This is a example showing init class
+
     Attributes:
         title (str): title movie .
-        storyline (str): this a brief about movie history 
+        storyline (str): this a brief about movie history
         poster_image_url (str): url who show the image poster movie
         trailer_youtube_url (str): rl who show the trailer movie
-    
+
     """
 
-	def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
-		self.title = title
-		self.storyline = storyline
-		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
+    def __init__(self, title, storyline,
+                 poster_image_url, trailer_youtube_url):
 
-	def show_trailer(self):
-		webbrowser.open(self.url_trailler)
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
 
-
-
+    def show_trailer(self):
+        """This function call the webPage who contains the movie trailer"""
+        webbrowser.open(self.url_trailler)
